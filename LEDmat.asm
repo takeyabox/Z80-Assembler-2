8b510c6dc1bd8e308111ddd9332bf0517048ba0e
takeyabox <takeyabox84@gmail.com>
2025-10-04 16:33:25 +0900

    add asm

---
+++ LEDmat.asm
@@ -0,0 +1,46 @@
+IOsetup:
+    LD A, 0x4F
+    OUT (02), A
+    LD A, 0x0F
+    OUT (03), A
+    RET
+
+display:
+    LD A, (0x00F0)
+    ADD A, 0x10
+    OUT (0x01), A
+    LD A, (0x00F1)
+    ADD A, 0x20
+    OUT (0x01), A
+    LD A, (0x00F2)
+    ADD A, 0x40
+    OUT (0x01), A
+    LD A, (0x00F3)
+    ADD A, 0x80
+    OUT (0x01), A
+    RET
+
+START:
+    CALL IOsetup
+    LD A, 0x00
+    LD B, 0x00
+
+LOOP:
+    LD (0x00F0), A
+    INC A
+    LD (0x00F1), A
+    INC A
+    LD (0x00F2), A
+    INC A
+    LD (0x00F3), A
+    INC A
+    CALL display
+    LD B, A
+    SUB A, 0x0F
+    LD A, B
+    JP NZ, LOOP
+    JP P, LOOP
+    JP START
+
+; End of program
+
